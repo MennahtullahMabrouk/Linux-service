91321f0aeb880bd76af3fba088e603cf41dec70b
MennahTullah Mabrouk <101124995+MennahMabrouk@users.noreply.github.com>
2024-03-01 17:46:47 +0200

    Add files via upload

---
+++ Zorin OS Service/linux.py
@@ -0,0 +1,19 @@
+import psutil
+import netifaces
+import time
+import os
+
+def collect_workload():
+    cpu_percent = psutil.cpu_percent()
+    memory_percent = psutil.virtual_memory().percent
+    disk_percent = psutil.disk_usage('/').percent
+    network_stats = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]
+    sent_bytes = network_stats['bytes_sent']
+    recv_bytes = network_stats['bytes_recv']
+    
+    return 'CPU usage: {}%\nMemory usage: {}%\nDisk usage: {}%\nBytes sent: {}\nBytes received: {}'.format(cpu_percent, memory_percent, disk_percent, sent_bytes, recv_bytes)
+
+while True:
+    with open(os.path.join(os.path.dirname(__file__), 'workload.txt'), 'a') as f:
+        f.write(f'{collect_workload()}\n')
+    time.sleep(60)
